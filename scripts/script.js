8d76e754196d8b510fd073f4341d7e4c86db4ed4
Givaldo Fernandes <gf.givaldofernandes@gmail.com>
2024-12-16 01:05:34 -0300

    New Site

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('.navbar_link, .navbar_logo').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
+
